d688ed58b2ff189d2b7fcda8797d4c71606407ba
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2023-01-17 14:11:51 -0800

    test: optimize tests for latest babel-plugin-tester

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,24 @@
-import pluginTester from 'babel-plugin-tester';
+import { pluginTester } from 'babel-plugin-tester';
 import plugin from '../src/index';
-import { join as makePath } from 'path';
-
-const babelOptions = {
-  parserOpts: { strictMode: true },
-  plugins: [
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-function-bind',
-    '@babel/plugin-transform-typescript'
-  ],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        // ? https://github.com/babel/babel-loader/issues/521#issuecomment-441466991
-        modules: false,
-        // ? https://nodejs.org/en/about/releases (see `npm show` lts version)
-        targets: { node: '12.22.7' }
-      }
-    ],
-    ['@babel/preset-typescript', { allowDeclareFields: true }]
-  ]
-};
 
 // eslint-disable-next-line jest/require-hook
 pluginTester({
-  pluginName: 'transform-default-named-imports',
   plugin,
-  babelOptions,
-  fixtures: makePath(__dirname, '__fixtures__')
+  fixtures: 'fixtures',
+  fixtureOutputExt: '.mjs',
+  babelOptions: {
+    parserOpts: { strictMode: true },
+    plugins: ['@babel/plugin-proposal-export-default-from'],
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // ? Leave import syntax alone
+          modules: false,
+          targets: 'maintained node versions'
+        }
+      ],
+      ['@babel/preset-typescript', { allowDeclareFields: true }]
+    ]
+  }
 });
